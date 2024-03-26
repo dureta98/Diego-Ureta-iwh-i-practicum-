3cbd4cddb4c1727d327760af37677efdfa602ae3
dureta98 <dureta@widefense.com>
2024-03-26 18:07:45 -0300

    test practicum

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// I don't have access to custom objects
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,25 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+
+// const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
+app.get('/', async (req, res) => {
     const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
@@ -41,31 +31,38 @@ app.get('/contacts', async (req, res) => {
     }
 });
 
-* * App.post sample
+// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+
+app.get('/forms', async (req, res) => {
+        res.render('index', { title: 'Contacts | HubSpot APIs'});      
+});
+
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+
 app.post('/update', async (req, res) => {
     const update = {
         properties: {
-            "favorite_book": req.body.newVal
+                    "email": req.body.correo,
+                    "firstname": req.body.nombre,
+                    "lastname": req.body.apellido,
+                    "phone": req.body.telefono,
         }
     }
 
-    const email = req.query.email;
+    const email = req.body.correo;
     const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
-    };
-
+    }
     try { 
         await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
+        res.redirect('/');
     } catch(err) {
         console.error(err);
     }
 
 });
-*/
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
